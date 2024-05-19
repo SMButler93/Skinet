6243b988a437c5fc445fc9c17e149764fabe226c
Scott Butler <sbutler@czarnikow.com>
2024-05-19 13:23:14 +0100

    initial commit

---
+++ Infrastructure/Data/SkinetDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class SkinetDbContext : DbContext
+    {
+        public SkinetDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SkinetWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Core.DomainModels;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkinetWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly SkinetDbContext _dbContext;
+
+        public ProductsController(SkinetDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async  Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
